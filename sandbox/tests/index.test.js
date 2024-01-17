55aeb0d85003052b4c2f0d818b8c74d76b3ebd86
Michael James <micrjames921@gmail.com>
2024-01-17 14:25:39 -0700

    setup code and tests

--- sandbox/tests/index.test.js
+++ sandbox/tests/index.test.js
@@ -1,7 +1,25 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs');
+const path = require('path');
+require('@testing-library/jest-dom');
+
 const { Form } = require("../dist/app/form/Form");
 
 describe("Form.", () => {
-   const login = new Form(document.forms[0]); 
+   let dom;
+   let document;
+   let login;
+   beforeAll(() => {
+	  const html = fs.readFileSync(path.resolve(__dirname, "..", "dist", "app", "views", 'index.ejs'), 'utf8');
+	  dom = new JSDOM(html, {runScripts: 'dangerously'});
+	  document = dom.window.document;
+	  login = new Form(document.forms[0]);
+   });
    test("Should be the form.", () => {
+	  expect(login.form).toBeInTheDocument();
+   });
+   test("Should output name on click.", () => {
+	  console.log(login.form.children);
    });
 });
